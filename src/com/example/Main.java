4a39c6883611275be577074d84598e93b92e211a
nitinvishwari <71441089+nitinvishwari@users.noreply.github.com>
2024-08-18 08:38:10 -0700

    day1

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,8 +1,11 @@
 package com.example;
+import nitin.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello bro");
+		SampleClass sampleNitin = new SampleClass();
+		sampleNitin.helper();
 	}
 }
