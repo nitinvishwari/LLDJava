190b399dfe441eed0483aeab88dcd73747b74fb2
Chinni Sai Harshith <csai-harshith@digital.ai>
2024-08-24 20:07:13 +0530

    new code chinni's

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -2,7 +2,12 @@ package com.example;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args1) {
+		System.out.println("Hello bro");
+	}
+	
+	//exploring method overloading
+	public static void main() {
 		System.out.println("Hello bro");
 	}
 }
